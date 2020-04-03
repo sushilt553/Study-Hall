2aa1e50bedfabff0864a12642bbc38a6adb74c56
Sushil <sushilt553@gmail.com>
2020-04-03 17:01:13 -0400

    Graphql query for category done

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,5 +1,35 @@
 import gql from "graphql-tag";
 
+export const FETCH_CATEGORIES = gql`
+    query Categories {
+        categories {
+            _id
+            name
+        }
+    }
+`
+
+export const FETCH_CATEGORY = gql`
+    query Category {
+        category {
+            _id
+            name
+            questions {
+                _id
+                title
+                answer {
+                    _id
+                    title
+                }
+                options {
+                    _id
+                    title
+                }
+            }
+        }
+    }
+`
+
 export const CURRENT_USER = gql`
     query CurrentUser {
         me {
