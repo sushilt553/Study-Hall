df1a32ec6140577488606e34d3376c4d24bbc678
Sushil <sushilt553@gmail.com>
2020-04-03 12:47:44 -0400

    Navbar CSS done

--- client/src/components/navbar/NavbarComponent.js
+++ client/src/components/navbar/NavbarComponent.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './assets/images/logo.png';
 import Modal from '../modal/Modal';
 import SignupForm from '../users/SignupForm';
 import LoginForm from '../users/LoginForm';
 import { useQuery } from '@apollo/react-hooks';
 import { IS_LOGGED_IN } from '../../graphql/queries';
 import LogOutButton from '../users/LogOutButton';
+import "./assets/navbar.css";
 
 export default () => {
 
@@ -16,28 +18,25 @@ export default () => {
     if (!data) return null;
 
     const buttons = data.isLoggedIn ? (
-        <li>
+        <div className="logout-button">
             <LogOutButton />
-        </li>
+        </div>
     ) : (
-            <ul>
-                <li>
-                    <button onClick={() => setModal("signup")}>
-                        Signup
-          </button>
-                </li>
-                <li>
-                    <button onClick={() => setModal("login")}>
-                        Login
-          </button>
-                </li>
-                {modal && <Modal component={modal === "signup" ? SignupForm : LoginForm} close={() => setModal(null)} />}
-            </ul>
+        <section className="signup-login-button">
+            <button className="login-button" onClick={() => setModal("login")}>
+                Log In
+            </button>
+            <button className="signup-button" onClick={() => setModal("signup")}>
+                Get Started
+            </button>
+            <div className="modal-component">{modal && <Modal component={modal === "signup" ? SignupForm : LoginForm} close={() => setModal(null)} />}</div>
+        </section >
         );
 
     return (
-        <>
+        <header className="navbar-header">
+            <img className="logo" src={Logo} />
             {buttons}
-        </>
+        </header>
     )
 }
