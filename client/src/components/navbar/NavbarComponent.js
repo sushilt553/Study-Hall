a93081effdb20abedca5863729310c92be3369c1
Sushil <sushilt553@gmail.com>
2020-04-02 20:01:45 -0400

    Create modal for login/signup

--- client/src/components/navbar/NavbarComponent.js
+++ client/src/components/navbar/NavbarComponent.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import Modal from '../modal/Modal';
+import SignupForm from '../users/SignupForm';
+import LoginForm from '../users/LoginForm';
 import { useQuery } from '@apollo/react-hooks';
 import { IS_LOGGED_IN } from '../../graphql/queries';
 import LogOutButton from '../users/LogOutButton';
 
 export default () => {
 
+    const [modal, setModal] = useState(null);
+
     const {data, loading, error} = useQuery(IS_LOGGED_IN);
 
     if (!data) return null;
 
-    const buttons = data.isLoggedIn ? 
+    const buttons = data.isLoggedIn ? (
+      <li>
+        <LogOutButton />
+      </li>
+    ) : (
+      <ul>
         <li>
-            <LogOutButton />
+          <button onClick={() => setModal("signup")}>
+            Signup
+          </button>
         </li>
-    :
-        <ul>
-            <li>
-                <Link to={`/signup`}>Signup</Link>
-            </li>
-            <li>
-                <Link to={`/login`}>Login</Link>
-            </li>
-        </ul>
+        <li>
+          <button onClick={() => setModal("login")}>
+            Login
+          </button>
+        </li>
+        {modal && <Modal component={modal === "signup" ? SignupForm : LoginForm} close={() => setModal(null)}/>}
+      </ul>
+    );
 
     return (
         <>
