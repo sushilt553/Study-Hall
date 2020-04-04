e81910eaaa93d8a1e400b1cac7b1f3899620a20d
Sushil <sushilt553@gmail.com>
2020-04-04 18:05:38 -0400

    Quiz start

--- client/src/components/category/CategoryShow.js
+++ client/src/components/category/CategoryShow.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useQuery} from '@apollo/react-hooks';
 import "./CategoryShow.css";
 import { FETCH_CATEGORY } from '../../graphql/queries';
 
 export default ({categoryId}) => {
+    
+    function checkAnswer(questionId, answer, answersList) {
+        if (answersList[questionId] === answer) {
+            setToggle("correct");
+        } else {
+            setToggle("incorrect");
+        }
+    }
+
+    const [toggle, setToggle] = useState("");
 
     const { data, loading, error } = useQuery(
         FETCH_CATEGORY,
@@ -17,7 +27,15 @@ export default ({categoryId}) => {
     if (loading) return <p>Loading...</p>
     if (error) return <p>ERROR</p>
     if(!data) return <p>Not Found</p>
-    if (!data.category) return <p>Category not found</p>
+    if (!data.category || !data.category.questions) return <p>Category not found</p>
+
+    const questionsArr = data.category.questions;
+    const answersList = {};
+
+    for (let i = 0; i < questionsArr.length; i++) {
+        debugger
+        answersList[questionsArr[i]._id] = questionsArr[i].answer.title;
+    };
 
     const questionsList = data.category.questions.map(question =>
         <>
@@ -25,8 +43,8 @@ export default ({categoryId}) => {
         { question.options.map(option =>
             <ul> 
                 <li>
-                    <input type="radio" id={option._id} name={question.title} value={option.title} />
-                    <label for={option._id}>{option.title}</label>
+                    <input  type="radio" id={option._id} name={question.title} value={option.title} onClick={() => checkAnswer(question._id, option.title, answersList )}/>
+                    <label className={toggle} htmlFor={option._id}>{option.title}</label>
                 </li>
             </ul>
             )}
