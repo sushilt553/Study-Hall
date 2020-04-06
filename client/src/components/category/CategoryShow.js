8075fc9d783c121e141ba11d79d42b709d44a890
Kadeem Jackson <kdjackson35@gmail.com>
2020-04-06 12:21:28 -0400

    Fix reset file not loading on catergory show page on refresh and add unique key to li tags

--- client/src/components/category/CategoryShow.js
+++ client/src/components/category/CategoryShow.js
@@ -1,89 +1,92 @@
-import React, {useState} from 'react';
-import {useQuery, useMutation} from '@apollo/react-hooks';
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import "./CategoryShow.css";
-import { FETCH_CATEGORY, CURRENT_USER } from '../../graphql/queries';
-import Question from './Question';
-import { UPDATE_POINT } from '../../graphql/mutations';
+import { FETCH_CATEGORY, CURRENT_USER } from "../../graphql/queries";
+import Question from "./Question";
+import { UPDATE_POINT } from "../../graphql/mutations";
+import "./reset.css";
 
-export default ({categoryId}) => { 
+export default ({ categoryId }) => {
+  const { data: dataR, error: errorR, loading: loadingR } = useQuery(
+    CURRENT_USER
+  );
 
-    const {data: dataR, error: errorR, loading: loadingR} = useQuery(CURRENT_USER);
+  if (!dataR || loadingR || errorR) return null;
 
-    if (!dataR || loadingR || errorR) return null;
+  const user = dataR.me;
 
-    const user = dataR.me;
-
-    const [updatePoint, {pointLoading, pointError}] = useMutation(
-        UPDATE_POINT,
-        {
-            refetchQueries: [{query: CURRENT_USER}]
-        }
-    )
-
-    if (pointLoading || pointError) return null;
-    
-    function checkAnswer(questionId, answer, answersList, setDisabled) {
-        if (answersList[questionId] === answer) {
-            setToggle("Correct");
-            setDisabled(true);
-            updatePoint({
-                variables: {
-                    point: 10
-                }
-            });
-            return true;
-        } else {
-            setToggle("Incorrect");
-            setDisabled(true);
-            updatePoint({
-                variables: {
-                    point: -10
-                }
-            });
-            return false
-        }
+  const [updatePoint, { pointLoading, pointError }] = useMutation(
+    UPDATE_POINT,
+    {
+      refetchQueries: [{ query: CURRENT_USER }],
     }
+  );
 
-    const [toggle, setToggle] = useState("");
+  if (pointLoading || pointError) return null;
 
-    const { data, loading, error } = useQuery(
-        FETCH_CATEGORY,
-        {
-            variables: {
-                categoryId: categoryId
-            }
-        }
-    )
+  function checkAnswer(questionId, answer, answersList, setDisabled) {
+    if (answersList[questionId] === answer) {
+      setToggle("Correct");
+      setDisabled(true);
+      updatePoint({
+        variables: {
+          point: 10,
+        },
+      });
+      return true;
+    } else {
+      setToggle("Incorrect");
+      setDisabled(true);
+      updatePoint({
+        variables: {
+          point: -10,
+        },
+      });
+      return false;
+    }
+  }
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>ERROR</p>
-    if(!data) return <p>Not Found</p>
-    if (!data.category || !data.category.questions) return <p>Category not found</p>
+  const [toggle, setToggle] = useState("");
 
-    const questionsArr = data.category.questions;
-    const answersList = {};
+  const { data, loading, error } = useQuery(FETCH_CATEGORY, {
+    variables: {
+      categoryId: categoryId,
+    },
+  });
 
-    for (let i = 0; i < questionsArr.length; i++) {
-        answersList[questionsArr[i]._id] = questionsArr[i].answer.title;
-    };
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not Found</p>;
+  if (!data.category || !data.category.questions)
+    return <p>Category not found</p>;
 
-    const questionsList = data.category.questions.map(question =>
-        <div className="quiz-header">
-            <Question answersList={answersList} question={question} checkAnswer={checkAnswer} setToggle={setToggle}/>
-        </div>
-    )
+  const questionsArr = data.category.questions;
+  const answersList = {};
 
-    return (
-        <section className="quiz-main">
-            <>You have {user.masteryPoints} Mastery Points</>
-            <div className="quiz-toggle-category">
-                <h1 className="quiz-category">{data.category.name}</h1>
-                <div className={toggle}>{toggle}</div>
-            </div>
-            <ol className="quiz-order-list">
-                {questionsList}
-            </ol>
-        </section>
-    )
+  for (let i = 0; i < questionsArr.length; i++) {
+    answersList[questionsArr[i]._id] = questionsArr[i].answer.title;
+  }
 
-}
+  const questionsList = data.category.questions.map((question, idx) => (
+    <div className="quiz-header" key={idx}>
+      <Question
+        answersList={answersList}
+        question={question}
+        checkAnswer={checkAnswer}
+        setToggle={setToggle}
+        key={question._id}
+      />
+    </div>
+  ));
+
+  return (
+    <section className="quiz-main">
+      <>You have {user.masteryPoints} Mastery Points</>
+      <div className="quiz-toggle-category">
+        <h1 className="quiz-category">{data.category.name}</h1>
+        <div className={toggle}>{toggle}</div>
+      </div>
+      <ol className="quiz-order-list">{questionsList}</ol>
+    </section>
+  );
+};
--- client/src/components/category/Question.js
+++ client/src/components/category/Question.js
@@ -1,25 +1,27 @@
-import React, {useState} from 'react';
-import Option from './Option';
+import React, { useState } from "react";
+import Option from "./Option";
+import "./CategoryShow.css";
 
+export default ({ question, checkAnswer, answersList, setToggle }) => {
+  const [disabled, setDisabled] = useState(false);
 
-export default({question, checkAnswer, answersList, setToggle}) => {
-
-    const [disabled, setDisabled] = useState(false);
-
-    return (
-        <>
-            <li className="quiz-title" disabled={disabled}>{question.title}</li>
-            {question.options.map(option =>
-                    <Option
-                        setToggle={setToggle}
-                        checkAnswer={checkAnswer}
-                        question={question}
-                        option={option}
-                        answersList={answersList}
-                        disabled={disabled}
-                        setDisabled={setDisabled}
-                    />
-            )}
-        </>
-    )
-}
+  return (
+    <>
+      <li className="quiz-title" disabled={disabled}>
+        {question.title}
+      </li>
+      {question.options.map((option, idx) => (
+        <Option
+          setToggle={setToggle}
+          checkAnswer={checkAnswer}
+          question={question}
+          option={option}
+          answersList={answersList}
+          disabled={disabled}
+          setDisabled={setDisabled}
+          key={idx}
+        />
+      ))}
+    </>
+  );
+};
