105d2afa1ad5e5206727d60164f67d76d0b1ff69
Sushil <sushilt553@gmail.com>
2020-04-06 18:14:30 -0400

    Sidebar complete

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default ({user, categories}) => {
 
     const categoriesList = categories.map(category => 
-        <li key={category._id}>{category.name}</li>
+        <li key={category._id}><Link to={`/category/${category._id}`}>{category.name}</Link></li>
         )
 
     return (
