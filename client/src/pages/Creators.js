335fbab770748b2c66166294b728a9a0d2c5bb7c
Kadeem Jackson <kdjackson35@gmail.com>
2020-04-07 01:05:41 -0400

    Style creators page

--- client/src/pages/Creators.js
+++ client/src/pages/Creators.js
@@ -5,18 +5,20 @@ export default () => {
   return (
     <div className="social-containers">
       <h1 className="social-name">Rapkat Amin</h1>
-      <label className="link-name">GitHub</label>
-      <div className="links-container">
-        <a href="https://github.com/rapkat10" className="GitHub-Logo">
-          <i className="fab fa-github" />
-        </a>
-        <label className="link-name">LinkedIn</label>
-        <a
-          href="https://www.linkedin.com/in/rapkat-amin-33b82b1a4/"
-          className="LinkedIn-Logo"
-        >
-          <i class="fab fa-linkedin" />
-        </a>
+      <div className="social-details-container">
+        <label className="link-name">GitHub</label>
+        <div className="links-container">
+          <a href="https://github.com/rapkat10" className="GitHub-Logo">
+            <i className="fab fa-github" />
+          </a>
+          <label className="link-name">LinkedIn</label>
+          <a
+            href="https://www.linkedin.com/in/rapkat-amin-33b82b1a4/"
+            className="LinkedIn-Logo"
+          >
+            <i className="fab fa-linkedin" />
+          </a>
+        </div>
       </div>
 
       <h1 className="social-name">Sushil Thapa</h1>
@@ -30,7 +32,7 @@ export default () => {
           href="https://www.linkedin.com/in/sushil-thapa-546800159/"
           className="LinkedIn-Logo"
         >
-          <i class="fab fa-linkedin" />
+          <i className="fab fa-linkedin" />
         </a>
       </div>
 
@@ -45,7 +47,7 @@ export default () => {
           href="https://www.linkedin.com/in/kadeem-jackson-4349348a/"
           className="LinkedIn-Logo"
         >
-          <i class="fab fa-linkedin" />
+          <i className="fab fa-linkedin" />
         </a>
       </div>
     </div>
