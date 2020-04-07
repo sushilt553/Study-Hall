801b1358776e0054954af5d2cd54c6441baf2235
Kadeem Jackson <kdjackson35@gmail.com>
2020-04-07 02:10:33 -0400

    Creators page and footer styling done

--- client/src/pages/Creators.js
+++ client/src/pages/Creators.js
@@ -6,15 +6,22 @@ export default () => {
     <div className="social-containers">
       <h1 className="social-name">Rapkat Amin</h1>
       <div className="social-details-container">
-        <label className="link-name">GitHub</label>
         <div className="links-container">
-          <a href="https://github.com/rapkat10" className="GitHub-Logo">
+          <a href="https://github.com/rapkat10" className="link-name">
+            GitHub
+          </a>
+          <a href="https://github.com/rapkat10" className="link-logo">
             <i className="fab fa-github" />
           </a>
-          <label className="link-name">LinkedIn</label>
           <a
             href="https://www.linkedin.com/in/rapkat-amin-33b82b1a4/"
-            className="LinkedIn-Logo"
+            className="link-name"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://www.linkedin.com/in/rapkat-amin-33b82b1a4/"
+            className="link-logo"
           >
             <i className="fab fa-linkedin" />
           </a>
@@ -22,33 +29,51 @@ export default () => {
       </div>
 
       <h1 className="social-name">Sushil Thapa</h1>
-      <label className="link-name">GitHub</label>
-      <div className="links-container">
-        <a href="https://github.com/sushilt553" className="GitHub-Logo">
-          <i className="fab fa-github" />
-        </a>
-        <label className="link-name">LinkedIn</label>
-        <a
-          href="https://www.linkedin.com/in/sushil-thapa-546800159/"
-          className="LinkedIn-Logo"
-        >
-          <i className="fab fa-linkedin" />
-        </a>
+      <div className="social-details-container">
+        <div className="links-container">
+          <a href="https://github.com/sushilt553" className="link-name">
+            GitHub
+          </a>
+          <a href="https://github.com/sushilt553" className="link-logo">
+            <i className="fab fa-github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sushil-thapa-546800159/"
+            className="link-name"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sushil-thapa-546800159/"
+            className="link-logo"
+          >
+            <i className="fab fa-linkedin" />
+          </a>
+        </div>
       </div>
 
       <h1 className="social-name">Kadeem Jackson</h1>
-      <label className="link-name">GitHub</label>
-      <div className="links-container">
-        <a href="https://github.com/Cro5s" className="GitHub-Logo">
-          <i className="fab fa-github" />
-        </a>
-        <label className="link-name">LinkedIn</label>
-        <a
-          href="https://www.linkedin.com/in/kadeem-jackson-4349348a/"
-          className="LinkedIn-Logo"
-        >
-          <i className="fab fa-linkedin" />
-        </a>
+      <div className="social-details-container">
+        <div className="links-container">
+          <a href="https://github.com/Cro5s" className="link-name">
+            GitHub
+          </a>
+          <a href="https://github.com/Cro5s" className="link-logo">
+            <i className="fab fa-github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kadeem-jackson-4349348a/"
+            className="link-name"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kadeem-jackson-4349348a/"
+            className="link-logo"
+          >
+            <i className="fab fa-linkedin" />
+          </a>
+        </div>
       </div>
     </div>
   );
