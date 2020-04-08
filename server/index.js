1d46d86ecaa868211cacdc22ef8df900b2e643c9
Sushil <sushilt553@gmail.com>
2020-04-08 12:34:32 -0400

    dockerfile/docker compose done

--- server/index.js
+++ server/index.js
@@ -39,6 +39,15 @@ app.use(
 
 app.get("/playground", expressPlayground({ endpoint: "/graphql"}));
 
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
